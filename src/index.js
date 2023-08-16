23e9ad1eafde559ab09c69fc2e55e66b60074f02
gfredex <g.fredex@gmail.com>
2023-08-16 14:58:25 +0300

    feat: added solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let newNum = '';
+    let strNum = String(Math.abs(n));
+    for (i = strNum.length - 1; i >= 0; i--) {
+        newNum += strNum[i];
+    }
+
+    return +newNum;
 }
